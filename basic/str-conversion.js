6bcaec161b9fb86ae3288d4d75a2dd29240eb758
KumarNarendra99 <narendrasaharan97@gmail.com>
2024-04-12 22:50:52 +0530

    string & datatype conversion

---
+++ basic/str-conversion.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Narendra"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+
+//console.log(1 + "2"); //12
+//console.log("1" + 2); //12
+//console.log("1" + 2 + 2); //122
+//console.log(1 + 2 + "2"); //32
+
+
+//DataType conversion
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log("2" === 2);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
